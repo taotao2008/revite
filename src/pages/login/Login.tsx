13de84cc0ba2b162d5264e7ae2e3ee9cfda377d8
taotao2008 <huangjuntao1987@126.com>
2023-03-29 18:45:05 +0800

    精简设置

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -27,6 +27,7 @@ export default observer(() => {
                 <Titlebar overlay />
             )}
             <div className={styles.login} style="text-align:center;">
+                <center>
                 <Helmet>
                     <meta
                         name="theme-color"
@@ -122,6 +123,7 @@ export default observer(() => {
                         </a>
                     </div>
                 </div>
+                </center>
             </div>
         </>
     );
