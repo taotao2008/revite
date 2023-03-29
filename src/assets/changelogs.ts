a66ba9886d4d39ff578ff264a8a002deb9fc4d9b
taotao2008 <huangjuntao1987@126.com>
2023-03-29 16:00:35 +0800

    精简设置

--- src/assets/changelogs.ts
+++ src/assets/changelogs.ts
@@ -14,19 +14,19 @@ export interface ChangelogPost {
 export const changelogEntries: Record<number, ChangelogPost> = {
     1: {
         date: new Date("2022-06-12T20:39:16.674Z"),
-        title: "Secure your account with 2FA",
+        title: "增加2FA安全账号功能",
         content: [
-            "Two-factor authentication is now available to all users, you can now head over to settings to enable recovery codes and an authenticator app.",
+            "增加双因素身份验证功能，可转到设置以启用恢复代码和身份验证器",
             {
                 type: "image",
                 src: "https://autumn.revolt.chat/attachments/E21kwmuJGcASgkVLiSIW0wV3ggcaOWjW0TQF7cdFNY/image.png",
             },
-            "Once enabled, you will be prompted on login.",
+            "启用后，系统会提示您登录",
             {
                 type: "image",
                 src: "https://autumn.revolt.chat/attachments/LWRYoKR2tE1ggW_Lzm547P1pnrkNgmBaoCAfWvHE74/image.png",
             },
-            "Other authentication methods coming later, stay tuned!",
+            "其他认证方式稍后推出，敬请期待!",
         ],
     },
 };
