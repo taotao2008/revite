4a48d952e740a3ed42bb13e73dacc0aeacd9f1b9
taotao2008 <huangjuntao1987@126.com>
2023-04-06 20:03:14 +0800

    to AiZen

--- vite.config.ts
+++ vite.config.ts
@@ -54,8 +54,8 @@ export default defineConfig({
             filename: "sw.ts",
             strategies: "injectManifest",
             manifest: {
-                name: "Revolt",
-                short_name: "Revolt",
+                name: "AiZen",
+                short_name: "AiZen",
                 description: "User-first, privacy-focused chat platform.",
                 categories: ["communication", "chat", "messaging"],
                 start_url: "/",
